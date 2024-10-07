c18bc39b17750b244d141802f81a4235c4e57256
Shaodi Yang <yangshaodi21@mails.ucas.ac.cn>
2024-10-07 15:39:51 +0800

    Changed project structure

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,23 @@
-from .context import sample
+# from .context import sample
+
+
+def foo():
+    print("foo")
+
+def dec(func):
+    def wrapper():
+        print("decorated")
+        func()
+        print("done")
+    return wrapper
+
+print(foo())
+print(dec(foo)())
+
+print("---------------")
+
+@dec
+def bar():
+    print("bar")
+
+print(bar())
